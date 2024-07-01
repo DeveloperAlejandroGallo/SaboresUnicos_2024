d84cd09fdc34e9d51b975d9f4cfdfc556d8baa78
MGonzalezx <martingonzalezt97@gmail.com>
2024-06-30 22:33:42 -0300

    Alta encuesta funcionando, falta que se pueda hacer una por dia

--- src/app/components/lista-encuestas/lista-encuestas.module.ts
+++ src/app/components/lista-encuestas/lista-encuestas.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ListaEncuestasComponent } from './lista-encuestas.component';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 @NgModule({
   imports: [ CommonModule, FormsModule , IonicModule],
   declarations: [ListaEncuestasComponent],
-  exports: [ListaEncuestasComponent]
+  exports: [ListaEncuestasComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ListaEncuestasModule { }
